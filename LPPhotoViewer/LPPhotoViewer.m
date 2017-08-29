5b5626e4c720ef9714910b7094c70276c47b62f2
litt1e-p <free9fw@gmail.com>
2017-08-29 14:00:38 +0800

    support NSURL/URL

--- LPPhotoViewer/LPPhotoViewer.m
+++ LPPhotoViewer/LPPhotoViewer.m
@@ -1,10 +1,24 @@
+// The MIT License (MIT)
 //
-//  LPPhotoViewer.m
-//  LPPhotoViewer
+// Copyright (c) 2017-2018 litt1e-p ( https://github.com/litt1e-p )
 //
-//  Created by litt1e-p on 16/3/27.
-//  Copyright © 2016年 litt1e-p. All rights reserved.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 #import "LPPhotoViewer.h"
 #import "LPPhotoView.h"
@@ -128,13 +142,19 @@
         CGRect frame = CGRectMake(index * _scrollView.frame.size.width, 0, self.view.frame.size.width, self.view.frame.size.height);
         id obj       = [self.imgArr objectAtIndex:index];
         if ([obj isKindOfClass:[NSString class]]) {
-            LPPhotoView *photoV = [[LPPhotoView alloc] initWithFrame:frame withPhotoUrl:obj];
+            LPPhotoView *photoV = [[LPPhotoView alloc] initWithFrame:frame withPhotoUrlStr:obj];
             photoV.delegate     = self;
             photoV.disableHorizontalDrag =  (self.imgArr.count > 1);
             [self.scrollView insertSubview:photoV atIndex:0];
             [_subViewList replaceObjectAtIndex:index withObject:photoV];
         } else if ([obj isKindOfClass:[UIImage class]]) {
             LPPhotoView *photoV = [[LPPhotoView alloc] initWithFrame:frame withPhotoImage:obj];
+            photoV.delegate     = self;
+            photoV.disableHorizontalDrag =  (self.imgArr.count > 1);
+            [self.scrollView insertSubview:photoV atIndex:0];
+            [_subViewList replaceObjectAtIndex:index withObject:photoV];
+        } else if ([obj isKindOfClass:[NSURL class]]) {
+            LPPhotoView *photoV = [[LPPhotoView alloc] initWithFrame:frame withPhotoUrl:obj];
             photoV.delegate     = self;
             photoV.disableHorizontalDrag =  (self.imgArr.count > 1);
             [self.scrollView insertSubview:photoV atIndex:0];
